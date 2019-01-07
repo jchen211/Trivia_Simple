381ce5e66836057d4f4428f87ede6d7946864ba4
Jessica Chen <jchen@jchen.local>
2019-01-06 18:23:37 -0600

    added questions as objects

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,73 @@
 $(document).ready(function() {
 
-    
+var triviaQuest = [
+    {
+        questions: "how many muscles do you have in total in all your fingers combined?",
+        answers: {
+            _ : "none",
+            _ : "ten",
+            _ : "twenty",
+            _ : "twenty-two"
+        },
+        correctAns: "none"
+    },
+
+    {
+        questions: "how many toes does a two toed sloth have?",
+        answers: {
+            _ : "none",
+            _ : "two or four",
+            _ : "six or eight",
+            _ : "ten"
+        },
+        correctAns: "two or four"
+    },
+
+    {
+        questions: "how many spaces are on a standard monopoly board?",
+        answers: {
+            _ : "twenty",
+            _ : "forty",
+            _ : "sixty",
+            _ : "eighty"
+        },
+        correctAns: "forty"
+    },
+
+    {
+        questions: "Which of these species are the most murderous mammals?",
+        answers: {
+            _ : "elk",
+            _ : "meerkats",
+            _ : "dogs",
+            _ : "chimpanzees"
+        },
+        correctAns: "meerkats"
+    },
+
+    {
+        questions: "how many dimples are there on a regular golf ball?",
+        answers: {
+            _ : "337",
+            _ : "418",
+            _ : "336",
+            _ : "294"
+        },
+        correctAns: "336"
+    },
+]
+
+
+
+function quizQuestions() {
+    var quiz = [];
+    var answers;
+
+    for (var i=0; i < questions.length; i++) {
+        answer = [];
+
+        for(in questions[i].answer)
+    }
+}
+
 });
