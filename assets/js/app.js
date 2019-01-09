fcf293664658c12b34d356b793cadf9ddac58315
Jessica Chen <jchen@jchen.local>
2019-01-08 18:32:36 -0600

    able to make questions hide and show

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,12 @@
 $(document).ready(function() {
 
-var triviaQuest = [
-
-    {
-        questions: "how many muscles do you have in total in all your fingers combined?",
-        answers: ["none", "ten", "twenty", "twenty-two"],
-        values: [true, false, false, false],
-    },
-
-    {
-        questions: "how many toes does a two toed sloth have?",
-        answers: ["none", "two or four", "six or eight", "ten"],
-        values: [false, false, true, false],
-    },
-
-    {
-        questions: "how many spaces are on a standard monopoly board?",
-        answers: ["twenty", "forty", "sixty", "eighty"],
-        values: [false, true, false, false],
-    },
-
-    {
-        questions: "Which of these species are the most murderous mammals?",
-        answers: ["elk", "meerkats", "dogs", "chimpanzees"],
-        values: [false, true, false, false],
-    },
-
-    {
-        questions: "how many dimples are there on a regular golf ball?",
-        answers: ["337", "418", "336", "294"],
-        values: [false, false, true, false],
-    },
-]
-
 var correct = 0;
 var wrong = 0;
 var noAns = 0;
+var intervalId; 
 
-var time = 120; 
-var timeCounter;
+time = 120;
+
 function timeConverter(t) {
 
     var minutes = Math.floor(t / 60);
@@ -58,21 +26,22 @@ function timeConverter(t) {
     return minutes + ":" + seconds;
 }
 
-function twoMin(){
-    timeCounter = setInterval (count, 1000);
+$('.row').hide();
+
+function startGame() {
+  $('#begin').click(function(){
+    $('.row').show();
+  });
+}
+
+setTimeout (twoMin, 1000 * 120);
+
+function twoMin() {
     timeConverter();
     time--;
     $('#timer').text("02:00");
 }
 
-function quizQuestions() {
-    var quiz = [];
-    var answers;
-
-    for (var i=0; i < questions.length; i++) {
-        answer = [];
-
-    }
-}
-
+twoMin();
+startGame();
 });
